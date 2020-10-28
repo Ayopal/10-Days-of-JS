d86df1d030ec39269da0d32be42ebe93ac4f8bc1
Ayoola Paul <Ayopaul19@gmail.com>
2020-10-28 20:29:21 +0100

    removed s variable

--- loops.js
+++ loops.js
@@ -2,7 +2,6 @@
  * Complete the vowelsAndConsonants function.
  * Print your output using 'console.log()'.
  */
-var s = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 function vowelsAndConsonants(s) {
     let vowels = ["a", "e", "i", "o", "u"];
